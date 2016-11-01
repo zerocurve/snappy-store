0c2a4f50f3f15fa0feb811a5597a29816fda757a
Sumedh Wale <swale@snappydata.io>
2016-11-01 15:53:44 +0530

    avoid NPEs when GemFireCache has been stopped

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/db/FabricDatabase.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/db/FabricDatabase.java
@@ -430,7 +430,7 @@ public final class FabricDatabase implements ModuleControl,
 
     try {
       final EmbedConnection embedConn = (EmbedConnection)conn;
-      final GemFireCacheImpl cache = this.memStore.getGemFireCache();
+      final GemFireCacheImpl cache = GemFireCacheImpl.getExisting();
       final LogWriter logger = cache.getLogger();
       final LanguageConnectionContext lcc = embedConn.getLanguageConnection();
       final GemFireTransaction tc = (GemFireTransaction)lcc
