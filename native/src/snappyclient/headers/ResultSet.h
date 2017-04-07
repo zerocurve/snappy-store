d5b0eb26acecbe472e2bf7f51716d992c704ba52
Sachin Kapse <skapse@snappydata.io>
2017-04-07 19:51:41 +0530

    git cherry-pick -n 928f7dda642a99c2c88b5e743b0fd0e4132fc9f6
    SNAP-1357: Update ODBC Driver Installer to working state.
     Fixes: Adding missing isBeforeFirst() function from earlier commit 008815a77cec16d0446c9144cbf4e60f42af9e14.

--- native/src/snappyclient/headers/ResultSet.h
+++ native/src/snappyclient/headers/ResultSet.h
@@ -299,6 +299,10 @@ namespace client {
         return m_currentRow != NULL;
       }
 
+      inline bool isBeforeFirst() const noexcept {
+        return m_currentRow == NULL && m_endBatch != NULL;
+      }
+
       /** Dereference the iterator. No NULL check like STL iterators. */
       TRow& operator*() const noexcept {
         return *m_currentRow;
